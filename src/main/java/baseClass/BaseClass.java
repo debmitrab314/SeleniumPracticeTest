877c558e659b82dc8cb057e24bcf464933c6a1d5
debmitrab314 <debmitrab314@gmail.com>
2020-02-13 14:35:15 +0530

    Master branch change

--- src/main/java/baseClass/BaseClass.java
+++ src/main/java/baseClass/BaseClass.java
@@ -1,5 +1,18 @@
 package baseClass;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
+	
+	
+	public BaseClass() 
+	{
+		WebDriver driver;
+		System.setProperty("Webdriver.Chrome.Driver","C:\\Users\\debmitra\\chromedriver_win32");
+		driver = new ChromeDriver();
+		driver.get("https://www.kantimehospice.net/");
+	}
+
 
 }
